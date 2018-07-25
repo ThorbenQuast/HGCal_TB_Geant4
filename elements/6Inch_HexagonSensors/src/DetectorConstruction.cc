bdeae492d4b0d61535c4f48d12c1433853ce1e9d
Thorben Quast <>
2018-07-25 23:16:41 +0200

    [wafer geometry] 109 full cells arranged in wafer

--- elements/6Inch_HexagonSensors/src/DetectorConstruction.cc
+++ elements/6Inch_HexagonSensors/src/DetectorConstruction.cc
@@ -23,6 +23,58 @@
 #include <vector>
 #include <cstdlib>
 #include <string>
+#include <cmath>
+
+
+  
+G4LogicalVolume* HexagonLogical(G4String name, G4double cellThickness, G4double cellSideLength, G4Material* material) {
+  G4double full_cellX = sqrt(3.) * cellSideLength;
+  G4double full_cellY = (2.) * cellSideLength;  
+  G4Box* solidFullcell = new G4Box(name,                       //its name
+       0.5*full_cellX, 0.5*full_cellY, 0.5*cellThickness);     //its size
+
+
+  G4double deltaXDash = cellSideLength;
+  G4double deltaYDash = sqrt(3)/4 * cellSideLength;
+  
+  G4Box* solidCutcell = new G4Box(name,                       //its name
+       0.5*deltaXDash, 0.5*(deltaYDash), 1.*cellThickness);     //its size                   
+
+
+  G4double DeltaTheta[4] = {60.*deg, 120.*deg, 240.*deg, 300.*deg};
+  G4double DeltaTheta_rot[4] = {30.*deg,150.*deg,210*deg,330*deg};
+  G4double Delta = sqrt(3)/2 * cellSideLength + deltaYDash/2;
+  
+  G4RotationMatrix* rot = new G4RotationMatrix;
+  rot->rotateZ(DeltaTheta_rot[0]);
+  std::vector<G4SubtractionSolid*> subtracted;
+  subtracted.push_back(new G4SubtractionSolid("cellSubtracted", solidFullcell, solidCutcell, rot, G4ThreeVector(cos(DeltaTheta[0])*Delta, sin(DeltaTheta[0])*Delta, 0.)));
+  
+  for (int i=1; i<4; i++) {
+    rot->rotateZ(-DeltaTheta_rot[i-1]);
+    rot->rotateZ(DeltaTheta_rot[i]);
+    subtracted.push_back(new G4SubtractionSolid("cellSubtracted", subtracted[i-1], solidCutcell, rot, G4ThreeVector(cos(DeltaTheta[i])*Delta, sin(DeltaTheta[i])*Delta, 0.)));  
+  }
+  
+  return new G4LogicalVolume(subtracted[3],          //its solid
+                        material,           //its material
+                        name);            //its name
+
+}
+
+
+
+G4PVPlacement* HexagonSensor(G4LogicalVolume* logicMother, G4String name, G4double cellThickness, G4double cellSideLength, G4Material* material, G4ThreeVector placement) {
+
+  return new G4PVPlacement(0,                     //no rotation
+                      placement,       //at (0,0,0)
+                      HexagonLogical(name, cellThickness, cellSideLength, material),            //its logical volume
+                      name,               //its name
+                      logicMother,                     //its mother  volume
+                      false,                 //no boolean operation
+                      0,                     //copy number
+                      true);        //overlaps checking  
+}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -54,7 +106,7 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
   //     
   // World
   //
-  G4double world_sizeXYZ = 50*cm;
+  G4double world_sizeXYZ = 20*cm;
   
   G4Box* solidWorld =    
     new G4Box("World",                       //its name
@@ -78,53 +130,52 @@ G4VPhysicalVolume* DetectorConstruction::Construct()
 
   //parameters for the hexagon
   G4double waferThickness = 0.3 * mm;
-  G4double waferSideLength = 1.2 * cm;
-
-
-
-  G4double full_waferX = sqrt(3.) * waferSideLength;
-  G4double full_waferY = (2.) * waferSideLength;  
-  G4Box* solidFullWafer = new G4Box("fullWafer",                       //its name
+  G4double cellSideLength = 1.2 * cm;
+  double alpha = 60./180. * M_PI;
+  double dx = 2*sin(alpha)*cellSideLength;
+  double dy = cellSideLength*(2.+2*cos(alpha));
+  
+  G4double full_waferX = 11*dx;
+  G4double full_waferY = 7*dy;
+  G4Material* wafer_mat = nist->FindOrBuildMaterial("G4_AIR");
+  G4Box* solidFullWafer = new G4Box("waferPhysical",                       //its name
        0.5*full_waferX, 0.5*full_waferY, 0.5*waferThickness);     //its size
 
-
-  G4double deltaXDash = waferSideLength;
-  G4double deltaYDash = sqrt(3)/4 * waferSideLength;
-  G4Box* solidCutWafer = new G4Box("fullWafer",                       //its name
-       0.5*deltaXDash, 0.5*(deltaYDash), 1.*waferThickness);     //its size                   
+  G4LogicalVolume* waferLogical = new G4LogicalVolume(solidFullWafer,          //its solid
+                        wafer_mat,           //its material
+                        "waferLogical");   
 
 
-  G4double DeltaTheta[4] = {60.*deg, 120.*deg, 240.*deg, 300.*deg};
-  G4double DeltaTheta_rot[4] = {30.*deg,150.*deg,210*deg,330*deg};
-  G4double Delta = sqrt(3)/2 * waferSideLength + deltaYDash/2;
+  G4Material* cell_mat = nist->FindOrBuildMaterial("G4_Si");
+
+  int index = 1;
   
-  G4RotationMatrix* rot = new G4RotationMatrix;
-  rot->rotateZ(DeltaTheta_rot[0]);
-  std::vector<G4SubtractionSolid*> subtracted;
-  subtracted.push_back(new G4SubtractionSolid("waferWithHole", solidFullWafer, solidCutWafer, rot, G4ThreeVector(cos(DeltaTheta[0])*Delta, sin(DeltaTheta[0])*Delta, 0.)));
+  int nRows[11] = {7, 6, 7, 6, 5, 6, 5, 4, 5, 4, 3};
   
-  for (int i=1; i<4; i++) {
-    rot->rotateZ(-DeltaTheta_rot[i-1]);
-    rot->rotateZ(DeltaTheta_rot[i]);
-    subtracted.push_back(new G4SubtractionSolid("waferWithHole", subtracted[i-1], solidCutWafer, rot, G4ThreeVector(cos(DeltaTheta[i])*Delta, sin(DeltaTheta[i])*Delta, 0.)));  
+  for (int nC=0; nC<11; nC++) {
+    for (int middle_index=0; middle_index<nRows[nC]; middle_index++){
+      HexagonSensor(waferLogical, ("cell_"+std::to_string(index++)).c_str(), waferThickness, cellSideLength, cell_mat, G4ThreeVector(nC*dx/2, dy*(middle_index-nRows[nC]/2.+0.5), 0.));
+      if (nC>0) HexagonSensor(waferLogical, ("cell_"+std::to_string(index++)).c_str(), waferThickness, cellSideLength, cell_mat, G4ThreeVector(-nC*dx/2, dy*(middle_index-nRows[nC]/2.+0.5), 0.));      
+    }
   }
+
+  G4VisAttributes* visAttributes = new G4VisAttributes(G4Colour(0.0,1.0,0.0));
+  visAttributes->SetVisibility(false);
+  waferLogical->SetVisAttributes(visAttributes);
+  
   
 
-  G4Material* fullWafer_mat = nist->FindOrBuildMaterial("G4_Si");
-  G4LogicalVolume* logicFullWafer =  new G4LogicalVolume(subtracted[3],          //its solid
-                        fullWafer_mat,           //its material
-                        "fullWafer");            //its name
+  //place the wafer
+  new G4PVPlacement(0,                     //no rotation
+      G4ThreeVector(0,0,0),       //at (0,0,0)
+      waferLogical,            //its logical volume
+      "waferPlacement",               //its name
+      logicWorld,                     //its mother  volume
+      false,                 //no boolean operation
+      0,                     //copy number
+      true);        //overlaps checking  
 
-  
-    new G4PVPlacement(0,                     //no rotation
-                      G4ThreeVector(0., 0., 0.),       //at (0,0,0)
-                      logicFullWafer,            //its logical volume
-                      "fullWafer",               //its name
-                      logicWorld,                     //its mother  volume
-                      false,                 //no boolean operation
-                      0,                     //copy number
-                      checkOverlaps);        //overlaps checking
-  
+
   fScoringVolume = logicWorld;
   
 
