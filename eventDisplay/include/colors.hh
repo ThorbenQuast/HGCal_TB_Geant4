feef0098822f93cd7ef46cc5ece0dc9de8bdd447
Thorben Quast <thorben.quast@cern.ch>
2019-09-22 22:30:59 +0200

    [eventDisplay] update colour map for MPV visualisation

--- eventDisplay/include/colors.hh
+++ eventDisplay/include/colors.hh
@@ -15,10 +15,10 @@ std::vector<float> AHCAL_green = {1.00, 0.5, 0.00, 0.81, 1.00, 0.20, 0.00};
 std::vector<float> AHCAL_blue = {1.00, 0.5, 0.51, 1.00, 0.12, 0.00, 0.00};
 
 
-std::vector<float> MIPMPV_scales = {0.0, 10.5, 30.5, 35.0, 40.0, 45., 60.};            //must start at 0 and must have at least two entries
-std::vector<float> MIPMPV_red = {1.00, 0.5, 0.00, 0.00, 0.87, 1.00, 0.51};
-std::vector<float> MIPMPV_green = {1.00, 0.5, 0.00, 0.81, 1.00, 0.20, 0.00};
-std::vector<float> MIPMPV_blue = {1.00, 0.5, 0.51, 1.00, 0.12, 0.00, 0.00};
+std::vector<float> MIPMPV_scales = {0.0, 10.5, 30.0, 35.0, 40.0, 50., 60.};            //must start at 0 and must have at least two entries
+std::vector<float> MIPMPV_red = {1.00, 0.5, 0.00, 0.10, 0.87, 1.00, 0.51};
+std::vector<float> MIPMPV_green = {1.00, 0.5, 1.00, 0.1, 1.00, 0.0, 0.50};
+std::vector<float> MIPMPV_blue = {1.00, 0.5, 0.01, 1.00, 0.12, 0.00, 0.50};
 
 void setHGCALHitColor (VisHit* hit) {
       float hit_energy = hit->energy;
@@ -70,7 +70,6 @@ void setMIPMPVColor (VisHit* hit) {
       unsigned short lower_index = MIPMPV_scales.size()-1;
       
       for (unsigned short i=0; i<MIPMPV_scales.size(); i++) {
-            std::cout<<hit_energy<<std::endl;
             if (hit_energy<MIPMPV_scales[i]) {
                   std::cout<<MIPMPV_scales[i]<<std::endl;
                   upper_index = i;
@@ -82,6 +81,5 @@ void setMIPMPVColor (VisHit* hit) {
       hit->red = upper_index!=lower_index ? (MIPMPV_red[upper_index]-MIPMPV_red[lower_index])/(MIPMPV_scales[upper_index]-MIPMPV_scales[lower_index]) * (hit_energy-MIPMPV_scales[lower_index])+MIPMPV_red[lower_index] : MIPMPV_red[lower_index];
       hit->green = upper_index!=lower_index ? (MIPMPV_green[upper_index]-MIPMPV_green[lower_index])/(MIPMPV_scales[upper_index]-MIPMPV_scales[lower_index]) * (hit_energy-MIPMPV_scales[lower_index])+MIPMPV_green[lower_index] : MIPMPV_green[lower_index];
       hit->blue = upper_index!=lower_index ? (MIPMPV_blue[upper_index]-MIPMPV_blue[lower_index])/(MIPMPV_scales[upper_index]-MIPMPV_scales[lower_index]) * (hit_energy-MIPMPV_scales[lower_index])+MIPMPV_blue[lower_index] : MIPMPV_blue[lower_index];
-      std::cout<<hit->red<<"  "<<hit->green<<"  "<<hit->blue<<std::endl;
 
 }
